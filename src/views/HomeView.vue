7fb28f3cd75b5883d8afadef08ae0bef44b3222d
soojung33 <jenny001012@naver.com>
2023-10-17 00:59:13 +0900

    2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,27 @@
 <template>
-  <div id="top">
-    <swiper :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
-      <swiper-slide style="background-image:url(/portfolio/img9.gif)" class="slide">
-        <div class="header">명지전문대 정보통신공학과 허수정</div>
-      </swiper-slide>
-
-      <swiper-slide style="background-image:url(/portfolio/img2.gif)" class="slide">
-        <div class="header">명지전문대 정보통신공학과 허수정</div>
-      </swiper-slide>
-
-      <swiper-slide style="background-image:url(/portfolio/img1.gif)" class="slide">
-        <div class="header">명지전문대 정보통신공학과 허수정</div>
-      </swiper-slide>
-
-    </swiper>
+  <div id="home" style="background-color: #000000; display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <v-img src="@/assets/main2.png" alt="Main Image" style="max-width: 60%; max-height: 60%;"></v-img>
   </div>
+
+  <div id="portfolio-skill">
+    <div class="header">S K I L L S</div>
+      <v-row justify="center">
+        <v-col cols="3" v-for="(item, index) in portfolioItems" :key="index">
+         <v-card class="custom-card">
+            <v-row no-gutters>
+              <v-col cols="12">
+               <v-img :src="item.image" class="item-image"></v-img>
+              </v-col>
+              <v-col cols="12">
+                <div class="item-name">{{ item.name }}</div>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+  </div>
+
+  <v-spacer style="background-color: #000000; height: 100px;"></v-spacer>
 
   <div id="portfolio">
     <div class="header"> 포트폴리오 </div>
@@ -48,11 +55,27 @@ import aos from "aos";
 import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
+  data() {
+  return {
+    portfolioItems: [
+      { name: 'Adobe Photoshop', image: require ('@/assets/adobe-photoshop.png')},
+      { name: 'Adobe Illustrator', image: require ('@/assets/adobe-AI.png')},
+      { name: 'Adobe XD', image: require ('@/assets/adobe-xd.png')},
+      { name: 'Figma', image: require ('@/assets/figma.png')},
+      { name: 'Html', image: require ('@/assets/html.png') },
+      { name: 'Java', image: require ('@/assets/java.png') },
+      { name: 'Vue-js', image: require ('@/assets/vue-js.png') },
+      { name: 'Wordpress', image: require ('@/assets/wordpress.png')},
+    ],
+  };
+},
+
   //swiper 컴포넌트 
   components: {
     Swiper,
     SwiperSlide,
   },
+
   setup() {
     const onSwiper = (swiper) => {
       console.log(swiper);
@@ -71,38 +94,69 @@ export default defineComponent({
   },
   methods: {
     movePage(path) {
-      this.$router.push(path);
+      this.$router.push('path');
     }
   },
   name: 'HomeView',
 
 });
+
 </script>
 
+
 <style lang="less">
-#top {
-  height: 90vh;
 
-  .swiper {
-    height: 80vh;
+#home {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start; /* 이미지를 위쪽으로 정렬 */
+  background-color: #000000;
+  margin-top: -2%; 
 
-    .slide {
-      background-position: center center;
-      background-size: cover;
-      text-align: center;
-      vertical-align: middle;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+}
 
-      .header {
-        font-size: 30px;
-        font-weight: 600;
-        display: inline-block;
-        height: 100%;
-        vertical-align: middle;
-        align-items: center;
-        color: #ffffff;
+#portfolio-skill {
 
-      }
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #ffffff;
+
+  .header {
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 5%;
+  }
+
+  .item-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
+
+  .item-image {
+    width: 70%;
+    height: 70%;
+    display: block; /* 이미지를 블록 요소로 만듭니다. */
+    margin: 0 auto; /* 이미지를 수평으로 가운데 정렬합니다. */
+}
+
+  .custom-card {
+    margin-bottom: 40%;
+    background-color: #000000;
+  }
+
+  .item-name {
+    font-weight: medium;
+    font-size: 15px;
+    color: #ffffff;
+    text-align: center; /* 텍스트를 가운데 정렬합니다. */
   }
 }
 
@@ -111,7 +165,8 @@ export default defineComponent({
     text-align: center;
     font-size: 30px;
     font-weight: bold;
-    margin-bottom: 50px;
+    margin-bottom: 10%;
+    margin-top: 10%;
   }
 
   width: 1000px;
@@ -127,9 +182,6 @@ export default defineComponent({
       background-position: center center;
       background-size: cover;
       border-radius: 20px;
-
-      //1:1 비율이 되도록
-      aspect-ratio: 1/1;
     }
 
   }
