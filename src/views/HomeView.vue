8ae8a589e56dcab502699a30eba1554c74a853f1
soojung33 <jenny001012@naver.com>
2023-10-19 11:42:24 +0900

    22

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,6 +23,34 @@
 
   <v-spacer style="background-color: #000000; height: 20vh;"></v-spacer>
 
+  <div id="silder">
+    <swiper :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
+  <swiper-slide class="slide">
+    <v-card class="custom-card">
+      <v-row justify="center">
+        <v-col cols="3">
+          <div class="header">PORTFOLIO.</div>
+          <div class="header2">BOOK & MUSIC <br> LOVERS (CLUB).</div>
+          <div class="content"> #Vue-js &nbsp; #Figma   <br><br> 책을 읽으면서 음악도 들을 수 있는 공간 <br> 본인의 책과 음원을 등록하고 수익을 얻을 수 있는 공간</div>
+        </v-col>
+        <v-spacer style="background-color: #ffffff; width: 10vh;"></v-spacer>
+        <v-col cols="8">
+          <v-img class="img" src="@/assets/B&L_sp1.png" style="max-width: 60%; max-height: 60%;"></v-img>
+        </v-col>
+      </v-row>
+    </v-card>
+  </swiper-slide>
+
+    <swiper-slide class="slide">
+      <!-- 이 슬라이드에 대한 내용 -->
+    </swiper-slide>
+
+    <swiper-slide class="slide">
+      <!-- 이 슬라이드에 대한 내용 -->
+    </swiper-slide>
+  </swiper>
+</div>
+
   <div id="portfolio">
     <div class="header"> 포트폴리오 </div>
 
@@ -155,6 +183,60 @@ export default defineComponent({
     font-size: 15px;
     color: #ffffff;
     text-align: center; /* 텍스트를 가운데 정렬합니다. */
+  }
+}
+
+#silder {
+  height: 120vh;
+  background-color: #ffffff;
+  justify-content: center;
+
+  .swiper {
+    height: 90vh;
+
+    .slide {
+      text-align: left;
+      margin-left: 18%;
+
+      .img {
+        margin-top: 8%;
+      }
+
+      .header {
+        margin-top: 35%;
+        font-size: 30px;
+        font-weight: bold;
+        display: inline-block;
+        color: #000000;
+        line-height: normal;
+      }
+
+      .header2 {
+        margin-top: 20%;
+        font-size: 35px;
+        font-weight: bold;
+        display: inline-block;
+        color: #000000;
+        line-height: normal;
+      }
+
+      .content {
+        margin-top: 40%;
+        font-size: 13px;
+        display: inline-block;
+        color: #000000;
+      }
+
+      .custom-card {
+  background-color: #E0E0E0; /* 배경색 설정 */
+  border-radius: 15px; /* 모서리를 둥글게 만듭니다 */
+  padding: 20px; /* 내용과 카드 경계 간의 여백을 설정합니다 */
+}
+
+.header, .header2, .content {
+  margin-bottom: 10px; /* 각 요소 간 여백을 설정합니다 */
+}
+    }
   }
 }
 
